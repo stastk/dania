3044153c03e26655f444dc20aa758c723fa52a08
Stas <me@stas.tk>
2023-04-20 00:54:47 +0300

    rewritten initdb & dropdb; added popdb

--- main.go
+++ main.go
@@ -9,6 +9,7 @@ import (
 
 	"main/models"
 
+	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
 )
 
@@ -18,13 +19,18 @@ func main() {
 	// Initalize the sql.DB connection pool and assign it to the models.DB
 	// global variable.
 	models.DB, err = sql.Open("postgres", "user=gouser password=gopass dbname=prania_exp sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
 
+	models.DBpr, err = sqlx.Connect("postgres", "user="+models.DBusername+" password="+models.DBusepassword+" dbname="+models.DBname+" sslmode=disable")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	http.HandleFunc("/initdb", initDB)
 	http.HandleFunc("/dropdb", dropDB)
+	http.HandleFunc("/popdb", populateDB)
 	http.HandleFunc("/ingridients", ingridientsIndex)
 	http.ListenAndServe(":3000", nil)
 
@@ -66,6 +72,20 @@ func dropDB(w http.ResponseWriter, r *http.Request) {
 // ingridientsIndex sends a HTTP response listing all ingridients.
 func initDB(w http.ResponseWriter, r *http.Request) {
 	answer, err := models.InitDB()
+	if err != nil {
+		log.Print(err)
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+
+	for _, answer := range answer {
+		fmt.Fprintf(w, "%s\n", answer)
+	}
+}
+
+// ingridientsIndex sends a HTTP response listing all ingridients.
+func populateDB(w http.ResponseWriter, r *http.Request) {
+	answer, err := models.PopulateDB()
 	if err != nil {
 		log.Print(err)
 		http.Error(w, http.StatusText(500), 500)
--- models/models.go
+++ models/models.go
@@ -9,18 +9,28 @@ import (
 	_ "github.com/lib/pq"
 )
 
-var schema = `
-CREATE TABLE person (
-    first_name text,
-    last_name text,
-    email text
-);
+var DBpr *sqlx.DB
+var DBname = "prania_exp"
+var DBusername = "gouser"
+var DBusepassword = "gopassword"
 
-CREATE TABLE place (
-    country text,
-    city text NULL,
-    telcode integer
-)`
+var DB *sql.DB
+
+var dbname = "prania_exp"
+
+var schema = `
+	CREATE TABLE IF NOT EXISTS Ingridients (
+		id serial PRIMARY KEY,
+		name VARCHAR(50) NOT NULL
+	);
+
+	CREATE TABLE IF NOT EXISTS IngridientsVariations (
+		id serial PRIMARY KEY,
+		name VARCHAR(50) NOT NULL,
+		ingridient_id int NOT NULL,
+		FOREIGN KEY (ingridient_id) REFERENCES Ingridients(id) ON UPDATE CASCADE
+	);
+	`
 
 // Temporary struct for getting Variations
 type IngridientWithVariations struct {
@@ -44,13 +54,9 @@ type Variation struct {
 
 // All Ingridients with their Variations
 func AllIngridients() ([]Ingridient, error) {
-	db, err := sqlx.Connect("postgres", "user=gouser password=gopass dbname=prania_exp sslmode=disable")
-	if err != nil {
-		log.Fatalln(err)
-	}
 
 	allthings := []Ingridient{}
-	rows, err := db.Queryx(`
+	rows, err := DBpr.Queryx(`
 		SELECT
 			i.id,
 			i.name,
@@ -59,6 +65,9 @@ func AllIngridients() ([]Ingridient, error) {
 			LEFT JOIN IngridientsVariations v ON v.ingridient_id = i.id
 			GROUP BY i.id;
 	`)
+	if err != nil {
+		log.Fatal(err)
+	}
 	defer rows.Close()
 
 	for rows.Next() {
@@ -87,19 +96,14 @@ func AllIngridients() ([]Ingridient, error) {
 
 }
 
-var DB *sql.DB
-
-var dbname = "prania_exp"
-
 var somevar []string
 
 // TODO solve problem with #empty_variables (somevar)
 // Drop all tables #db
 func DropDB() ([]string, error) {
 	rows, err := DB.Query(`
-		DROP TABLE IF EXISTS IngridientsToIngridientsVariations;
-		DROP TABLE IF EXISTS Ingridients;
 		DROP TABLE IF EXISTS IngridientsVariations;
+		DROP TABLE IF EXISTS Ingridients;
 	`)
 	if err != nil {
 		return somevar, err
@@ -110,48 +114,36 @@ func DropDB() ([]string, error) {
 	return somevar, err
 }
 
-// Create all tables #db
 func InitDB() ([]string, error) {
-	rows, err := DB.Query(`
-	CREATE TABLE IF NOT EXISTS Ingridients (
-		id serial PRIMARY KEY,
-		name VARCHAR(50) NOT NULL
-	);
 
-	CREATE TABLE IF NOT EXISTS IngridientsVariations (
-		id serial PRIMARY KEY,
-		name VARCHAR(50) NOT NULL,
-		  ingridient_id int NOT NULL,
-		FOREIGN KEY (ingridient_id) REFERENCES Ingridients(id) ON UPDATE CASCADE
-	);
+	DBpr.MustExec(schema)
+	return somevar, nil
 
-	INSERT INTO Ingridients
-		("id", "name")
+}
+
+func PopulateDB() ([]string, error) {
+	tx := DBpr.MustBegin()
+	tx.MustExec(`
+		INSERT INTO Ingridients
+			(name)
 		VALUES
-		(1, 'Ganash'),
-		(2, 'Salt'),
-		(3, 'Pepper'),
-		(4, 'Milk');
-		
-	INSERT INTO IngridientsVariations
-		("id", "name", "ingridient_id")
-		VALUES
-		(1, 'Ganash of the north', 1),
-		(2, 'Ganash of the south', 1),
-		(3, 'Salt of the sea', 2),
-		(4, 'Ganash of the west', 1),
-		(5, 'Pepper of the Iron Man', 3),
-		(6, 'Ganash of the north', 1),
-		(7, 'Ganash of the east', 1),
-		(8, 'Dr.Pepper', 3);
-		
+			('Ganash'),
+			('Salt'),
+			('Pepper'),
+			('Milk');
+
+		INSERT INTO IngridientsVariations
+			(name, ingridient_id)
+		VALUES 
+			('Ganash of the north', 1),
+			('Ganash of the south', 1),
+			('Salt of the sea', 2),
+			('Ganash of the west', 1),
+			('Pepper of the Iron Man', 3),
+			('Ganash of the north', 1),
+			('Ganash of the east', 1),
+			('Dr.Pepper', 3);
 	`)
-
-	if err != nil {
-		return somevar, err
-	}
-
-	defer rows.Close()
-
-	return somevar, err
+	tx.Commit()
+	return somevar, nil
 }
